90784c3ddd9b527c645141eab3ec6cc47ee12d07
raacker <haven.cpp@gmail.com>
2016-07-30 23:17:46 +0900

    Added apt-get update and README.md

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Haven's Blog development Automatic setting
 
 Need more : download atom and jekyll plug-in
+
+
+Usage:
+
+chmod run.sh and just run it
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get update
 apt-get install -y curl
 apt-get install -y nodejs
 
@@ -13,4 +14,5 @@ gem install jekyll
 gem install bundler
 gem install jekyll-paginate
 
+cd ~
 git clone https://github.com/raacker/raacker.github.io
